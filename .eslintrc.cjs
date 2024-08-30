b15c2566f4ad1f03f3cf14da4cac86c96ecf5334
wijayanto1320 <wijayanto1320@gmail.com>
2024-08-30 07:27:07 +0700

    chore: Update project configuration files and add GitHub issue templates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser", 
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
@@ -21,19 +21,22 @@ module.exports = {
   extends: [
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
-    "prettier", 
-    "plugin:tailwindcss/recommended" 
+    "prettier",
+    "plugin:tailwindcss/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
   plugins: [
-    "react", 
-    "jsx-a11y", 
-    "@typescript-eslint", 
-    "import", 
-    "prettier" 
+    "react",
+    "jsx-a11y",
+    "@typescript-eslint",
+    "import",
+    "prettier"
   ],
   settings: {
     react: {
-      version: "detect", 
+      version: "detect",
     },
     formComponents: ["Form"],
     linkComponents: [
@@ -48,6 +51,59 @@ module.exports = {
     },
   },
   rules: {
+        '@typescript-eslint/naming-convention': [
+      'error',
+      // Variabel umum (camelCase)
+      {
+        selector: 'variable',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow', // Izinkan awalan underscore (misal: _privateVar)
+      },
+      // Konstanta (UPPER_CASE)
+      {
+        selector: 'variable',
+        types: ['boolean', 'string', 'number'],
+        format: ['UPPER_CASE'],
+      },
+      // Parameter Fungsi (camelCase)
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+
+      // Penamaan Class (PascalCase)
+      {
+        selector: 'class',
+        format: ['PascalCase'],
+      },
+
+      // Penamaan Fungsi (camelCase atau PascalCase untuk constructor)
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      // Method (camelCase atau PascalCase jika static)
+      {
+        selector: 'method',
+        format: ['camelCase', 'PascalCase'],
+      },
+      // Property (camelCase atau PascalCase jika static)
+      {
+        selector: 'property',
+        format: ['camelCase', 'PascalCase'],
+      },
+      // Type Alias (PascalCase)
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+      },
+      // Interface (PascalCase)
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+      },
+    ],
     // Aturan dari file ESLint 2
     "import/consistent-type-specifier-style": ["warn", "prefer-inline"],
     "import/no-duplicates": ["warn", { "prefer-inline": true }],
@@ -67,10 +123,10 @@ module.exports = {
         disallowTypeAnnotations: true,
       },
     ],
-    
+
     // Aturan tambahan (bisa dari file ESLint 1 atau kustom)
     "prettier/prettier": "error",  // Pastikan error Prettier ditampilkan
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }], 
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
     "react-hooks/exhaustive-deps": "warn" // Contoh aturan tambahan
   },
   overrides: [
